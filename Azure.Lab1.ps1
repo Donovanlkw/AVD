31d3cd413e6fa0633b00fdab21dacba411ae3cc4
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-05-18 15:22:05 +0800

    Update Azure.Lab1.ps1

--- Azure.Lab1.ps1
+++ Azure.Lab1.ps1
@@ -1,13 +1,22 @@
+################ Definition of Primary site ##################
+
 $Location = 'EastAsia'
 $ResourceGroupName = 'MyResourceGroup'
+$ResourceGroupNameNET = 'MyResourceGroupNET'
 $vnet = 'MyVNet'
 $VMname= 'AVD11'
 
+################ Create a ResrouceGroup ##################
+New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+New-AzResourceGroup -Name $ResourceGroupNameNET -Location $Location
+
+
+################ Create a Virtual Network ##################
 $vnet = @{
     Name = $vnet
-    ResourceGroupName = $ResourceGroupName
+    ResourceGroupName = $ResourceGroupNameNET
     Location = $Location
-    AddressPrefix = '10.0.0.0/16'    
+    AddressPrefix = '10.0.0.0/8'    
 }
 $virtualNetwork = New-AzVirtualNetwork @vnet
 
@@ -17,12 +26,19 @@ $subnet = @{
     AddressPrefix = '10.0.0.0/24'
 }
 $subnetConfig = Add-AzVirtualNetworkSubnetConfig @subnet
+$virtualNetwork | Set-AzVirtualNetwork
+
+################ Create a Storage Account ################ 
+New-AzStorageAccount -ResourceGroupName $ResourceGroupName -AccountName sabs999999999a -Location $Location -SkuName Standard_GRS -Kind BlobStorage
+New-AzStorageAccount -ResourceGroupName $ResourceGroupName -AccountName sabb9999999999a -Location $Location -SkuName Standard_GRS -Kind BlockBlobStorage
+New-AzStorageAccount -ResourceGroupName $ResourceGroupName -AccountName safs9999999999a -Location $Location -SkuName Standard_GRS -Kind FileStorage
+New-AzStorageAccount -ResourceGroupName $ResourceGroupName -AccountName sav19999999999a -Location $Location -SkuName Standard_GRS -Kind Storage
+New-AzStorageAccount -ResourceGroupName $ResourceGroupName -AccountName sav29999999999a -Location $Location -SkuName Standard_GRS -Kind StorageV2
 
 
 
-$Location = 'EastAsia'
-$ResourceGroupName = 'MyResourceGroup'
-$vnet = 'MyVNet'
+
+
 $vmName = 'vm-lab-win001'
 
 ################ Create a VM Configuration ################ 
@@ -30,14 +46,3 @@ $vmConfig = New-AzVMConfig -VMName $vmName -VMSize $VMSize -AvailabilitySetID $A
 Set-AzVMOperatingSystem -Windows -ComputerName $vmName -Credential $Cred | `
 Set-AzVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus 2019-Datacenter -Version latest | `
 Add-AzVMNetworkInterface -Id $nic.Id
-
-
-
-
-New-AzResourceGroup -Name $ResourceGroupName -Location $Location
-
-New-AzStorageAccount -ResourceGroupName MyResourceGroup -AccountName sa8765456789876 -Location westus -SkuName Standard_GRS
-
-New-AzStorageAccount -ResourceGroupName MyResourceGroup -AccountName sa000gytghjnhg -Location westus -SkuName Standard_GRS -Kind BlobStorage -AccessTier Hot
-
-New-AzStorageAccount -ResourceGroupName MyResourceGroup -AccountName sa0005y6u7yjhgn -Location westus -SkuName Standard_GRS -Kind StorageV2 -AssignIdentity
