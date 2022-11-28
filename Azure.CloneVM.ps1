e53099e8e0459ab1147bb3e713ae21637a64960a
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-11-28 17:00:42 +0800

    Update Azure.CloneVM.ps1

--- Azure.CloneVM.ps1
+++ Azure.CloneVM.ps1
@@ -43,13 +43,74 @@ $vm = Set-AzVMOSDisk -VM $vm -ManagedDiskId $osDisk.Id -StorageAccountType Stand
 New-AzVM -ResourceGroupName $destinationResourceGroup -Location $location -VM $vm
 
 
-####Powershell run inside the windwos 10.
 
-
+### Powershell run inside the windwos 10.
+### === 1st reboot === ###
 $userid = "tmpadmin"
 $Password = "Password1"
-
+$Encryptedpassword=$Password | ConvertTo-SecureString -Force -AsPlainText
 New-LocalUser $userid -Password $Password -FullName "tmp adm" -Description "tmp adm for Cloning"
 Add-LocalGroupMember -Group "Administrators" -Member $userid
-Get-AppxPackage -all * | Remove-AppxPackage -AllUsers 
-%WINDIR%\system32\sysprep\sysprep.exe /generalize /reboot
+Get-AppxPackage | Remove-AppxPackage -AllUsers 
+C:\Windows\system32\sysprep\sysprep.exe /generalize /oobe /reboot
+
+
+### === 2nd reboot === ###
+$Domainuserid = "mfcgd\leekwun"
+$userid = "tmpadmin"
+$Password = "Password1"
+$Domain= "MFCGD.COM"
+$OUPath= "OU=VDI,OU=Win10,OU=Hong Kong,OU=Managed Computers,DC=MFCGD,DC=COM"
+Set-ExecutionPolicy Unrestricted -force
+add-computer -NewName $Newname –domainname $domain -OUPath $OUPath -Credential $domainuserid 
+Remove-LocalUser -name $userid
+Set-ExecutionPolicy restricted
+shutdown -r -t 0
+###################################################################################################
+
+
+
+$file = "c:\$task.ps1"
+
+### Create a auto run RDS Configuration
+Get-ScheduledTask
+$resumeActionscript = "-WindowStyle Normal -NoLogo -NoProfile -File $file"
+$act = New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument $resumeActionscript
+$trig = New-ScheduledTaskTrigger -AtStartup
+Register-ScheduledTask -TaskName $task -user $user -password $pwd -Action $act -Trigger $trig -RunLevel Highest
+
+
+### Create a Powershell script file for configure RDS script
+New-Item $file -force
+Set-Content $file '$BrokerServer="' -NoNewline
+Add-Content $file "$BrokerServer" -NoNewline
+Add-Content $file '"'
+
+Add-Content $file '$CollectionName="' -NoNewline
+Add-Content $file "$CollectionName" -NoNewline
+Add-Content $file '"'
+
+Add-Content $file '
+$NewBrokerServer= [System.Net.Dns]::GetHostByName($env:computerName).HostName
+$NewSessionServer= [System.Net.Dns]::GetHostByName($env:computerName).HostName
+Import-Module ServerManager
+Add-RDServer -Server $NewBrokerServer -Role "RDS-CONNECTION-BROKER" -ConnectionBroker $BrokerServer *>&1 >> C:\temp\output1.txt
+Add-RDServer -Server $NewSessionServer -Role "RDS-RD-SERVER" -ConnectionBroker $BrokerServer *>&1 >> C:\temp\output2.txt
+Add-RDSessionHost -CollectionName $CollectionName -SessionHost $NewSessionServer -ConnectionBroker $BrokerServer *>&1 >> C:\temp\output3.txt
+Set-RDSessionHost -SessionHost $NewSessionServer  -NewConnectionAllowed "No" -ConnectionBroker $BrokerServer 
+' 
+
+Add-Content $file 'Unregister-ScheduledTask '  -NoNewline 
+Add-Content $file "$task " -NoNewline
+Add-Content $file '-Confirm:$false' 
+
+Add-Content $file '
+### Restore security configuration
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\"  -Name "MinEncryptionLevel" -Value "3"  -PropertyType "DWORD"
+set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\" -Name "DisableDomainCreds" -value "1"
+set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -value "1"
+### Restore security policy configuration
+secedit /configure /db C:\Windows\security\local.sdb /areas USER_RIGHTS /cfg  c:\temp\secpolfinal.inf
+c:\temp\Ericom\install.ps1
+Restart-Computer
+'
